3e55729fc2f0e2fb2c28b68082f86ecc84864927
Lucas Moraes <lucas_moraes2011@outlook.com>
2022-03-29 01:47:58 -0300

    refactor: removed unnecessary files

--- packages/ui/helper/useFetcher.tsx
+++
@@ -1,6 +0,0 @@
-import useSWR from 'swr'
-export function useFetch<Data = any, Error = any>(url: string) {
-    const fetcher = (url: string) => fetch(url).then((res: any) => res.json());
-    const { data, error } = useSWR(url, fetcher);
-    return { data, error }
-  }
